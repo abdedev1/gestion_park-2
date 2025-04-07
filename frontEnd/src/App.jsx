4797c2fe0242b2d3618a2b57bee26cc8b7142840
ii-Zenitsu <mohammedmmmm348@gmail.com>
2025-04-08 00:02:46 +0100

    refactor: remove unused logos and update App component layout

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button class="btn btn-soft">Default</button>
+      <button class="btn btn-soft btn-primary">Primary</button>
+      <button class="btn btn-soft btn-secondary">Secondary</button>
+      <button class="btn btn-soft btn-accent">Accent</button>
+      <button class="btn btn-soft btn-info">Info</button>
+      <button class="btn btn-soft btn-success">Success</button>
+      <button class="btn btn-soft btn-warning">Warning</button>
+      <button class="btn btn-soft btn-error">Error</button>
     </>
   )
 }
