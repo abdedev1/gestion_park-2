157a12a95364ea9e83b8b98318262b65bfd7126c
abderahim <abderahimbensaid111@gmail.com>
2025-04-08 14:15:46 +0100

    admin dashborad

--- backEnd/routes/api.php
+++ backEnd/routes/api.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get("/users", [UserController::class, 'index']);
+    
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 Route::post("/register", [AuthController::class, 'register']);
 Route::post("/login", [AuthController::class, 'login']);
-Route::post("/users", [UserController::class, 'index']);
+Route::get("/users", [UserController::class, 'index']);
