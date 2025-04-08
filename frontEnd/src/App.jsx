cfdc611e7e2d1b355a5c5ded732916456e53d653
ii-Zenitsu <mohammedmmmm348@gmail.com>
2025-04-08 16:01:31 +0100

    delete node_module

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-// import FormTabs from './components/login/Signup'
+import FormTabs from './components/login/Signup'
 import './App.css'
 import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import Header from './component/admin/Header'
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <>
-      <Header/>
-      {/* <FormTabs/> */}
+      {/* <Header/> */}
+      <FormTabs/>
     </>
   )
 }
