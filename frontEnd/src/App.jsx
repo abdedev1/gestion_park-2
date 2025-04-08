d0053786e14913b32140e6c481edafc15fefccc5
abderahim <abderahimbensaid111@gmail.com>
2025-04-08 14:14:18 +0100

    admin dashborad

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import Header from './component/admin/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <button class="btn btn-soft">Default</button>
-      <button class="btn btn-soft btn-primary">Primary</button>
-      <button class="btn btn-soft btn-secondary">Secondary</button>
-      <button class="btn btn-soft btn-accent">Accent</button>
-      <button class="btn btn-soft btn-info">Info</button>
-      <button class="btn btn-soft btn-success">Success</button>
-      <button class="btn btn-soft btn-warning">Warning</button>
-      <button class="btn btn-soft btn-error">Error</button>
+      <BrowserRouter>
+        <Header/>
+      
+      
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontEnd/src/component/admin/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div className="dashboard">
+        <h1>Dashboard</h1>
+        <p>Welcome to the admin dashboard!</p>
+        </div>
+    );
+    }
