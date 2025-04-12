a31ec050338c318266410581ebf42370f09dbd1e
ii-Zenitsu <mohammedmmmm348@gmail.com>
2025-04-13 00:28:49 +0100

    Enhance Header component: add dynamic user color based on first name, improve avatar display, and update welcome message.

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { use, useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,6 +11,9 @@ export default function Header() {
     const navigate = useNavigate();
     const { user, token} = useSelector((state) => state.auth);
     const [isOpen, setIsOpen] = useState(false);
+    let color = "primary"
+    if (token)
+    {color = getColor(user.first_name);}
     
     const logoutUser = async () => {
       const res = await Auth.Logout();
@@ -19,6 +22,16 @@ export default function Header() {
         navigate("/");
       }
     };
+
+    function getColor(str) {
+      const colors = ["neutral", "primary", "secondary", "accent", "info", "success", "warning", "error"]
+      let hash = 0;
+      for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+      }
+      const index = Math.abs(hash) % colors.length;
+      return colors[index];
+    }
 
     return (
     <>
@@ -35,18 +48,24 @@ export default function Header() {
           </div>
           <div className="navbar-end">
           <button className="btn btn-ghost btn-neutral btn-circle md:hidden hover:scale-105 transition-transform duration-100" onClick={() => setIsOpen(!isOpen)}><Menu/></button>
+          
+          { token ?
             <div className="dropdown dropdown-end hidden md:inline-block">
-              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar hover:scale-105 transition-transform duration-100">
+                <div className={`hover:ring ring-offset-2 ring-neutral ring-offset-base-100 w-10 rounded-full bg-${color} text-${color}-content flex! items-center justify-center text-lg font-bold`}>
+                  {user.first_name[0]}{user.last_name[0]}
                 </div>
               </div>
               <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                <li className='text-center font-semibold mb-2'>Welcome {user.first_name}</li>
                 <li><a className="justify-between">Profile</a></li>
                 <li><a>Settings</a></li>
                 <li><button onClick={logoutUser}>Logout</button></li>
               </ul>
             </div>
+            : <NavLink className={({ isActive }) => `btn btn-sm btn-neutral mx-2 hover:bg-base-100 hover:text-neutral ${isActive ? "btn-active" : ""}`} to="/sign">Login</NavLink>
+          }
+          
           </div>
         </div>
         {/* sidebar */}
@@ -54,15 +73,16 @@ export default function Header() {
         <aside className={`fixed top-0 left-0 w-full h-fit bg-white shadow-lg p-4 transform transition-transform ${isOpen ? "translate-y-0" : "-translate-y-full"}`}>
           <div className='flex justify-between items-center mb-2'>
             <div className="btn btn-ghost btn-circle avatar hover:scale-105 transition-transform duration-100">
-              <div className="hover:ring ring-offset-2 ring-neutral ring-offset-base-100 w-10 rounded-full">
+              <div className={`hover:ring ring-offset-2 ring-neutral ring-offset-base-100 w-10 rounded-full bg-${color} text-${color}-content flex! items-center justify-center text-lg font-bold`}>
                 <NavLink to="/profile">
-                  <img alt="avatar" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                {user.first_name[0]}{user.last_name[0]}
                 </NavLink>
               </div>
             </div>
             <button className="btn btn-ghost btn-neutral btn-circle hover:scale-105 transition-transform duration-100" onClick={() => setIsOpen(false)}><X/></button>
           </div>
           <div className='flex flex-col items-center gap-2 px-2'>
+            <div className='text-center font-semibold mb-2'>Welcome {user.first_name}</div>
             <NavLink className={({ isActive }) => `btn btn-ghost btn-neutral mx-2 w-full ${isActive ? "btn-active" : ""}`} to="/admin/users">Users</NavLink>
             <NavLink className={({ isActive }) => `btn btn-ghost btn-neutral mx-2 w-full ${isActive ? "btn-active" : ""}`} to="/admin/roles">Roles</NavLink>
             <NavLink className={({ isActive }) => `btn btn-ghost btn-neutral mx-2 w-full ${isActive ? "btn-active" : ""}`} to="/admin/SettingsAdmin">Settings</NavLink>
