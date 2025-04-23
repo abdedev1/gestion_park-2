7d686e00bfe17b420bed87bbfb4f096fee676ff2
ii-Zenitsu <mohammedmmmm348@gmail.com>
2025-04-23 19:18:13 +0100

    feat: Update title in index.html and enhance ParkingTickets component with search and filter functionality

--- frontEnd/src/components/admin/ParkingTickts.jsx
+++ frontEnd/src/components/admin/ParkingTickts.jsx
@@ -15,10 +15,21 @@ export default function ParkingTickets() {
   const [form] = Form.useForm()
   const [editForm] = Form.useForm()
   const [messageApi, contextHolder] = message.useMessage()
-  const TicketFuse = new Fuse(parkingT, {
-    keys: [ "clientName", "entry_time", "exit_time", "status", "base_rate_id", "total_price", "client_id"],})
 
+  const [query, setQuery] = useState("");
+  const [types, setTypes] = useState([]);
 
+  const ticketsFuse = new Fuse(getRes(), {keys: ["client_first_name", "client_last_name"], threshold: 0.3})
+  const items = query ? ticketsFuse.search(query).map(r => r.item) : getRes();
+
+  function getRes() {
+    let res = types.length ? parkingT.filter(c => types.includes(c.status)) : parkingT
+    return res
+  }
+
+  function toggleType(type) {
+    types.includes(type) ? setTypes(types.filter(t => t !== type)) : setTypes([...types, type])
+  }
 
   // Fetch roles from API
   const fetchTickets = async () => {
@@ -197,13 +208,23 @@ export default function ParkingTickets() {
           <h1 className="text-2xl font-bold text-gray-800">Parking Tickets Management</h1>
          
         </div>
+        <div className="flex justify-end p-3 gap-3">
+        <div className="flex gap-1.5">
+          <button className={`btn btn-outline px-4 btn-primary border-primary/50 btn-sm rounded-full capitalize ${types.includes("active") ? "btn-active" : ""}`} onClick={() => toggleType("active")}>active</button>
+          <button className={`btn btn-outline px-4 btn-primary border-primary/50 btn-sm rounded-full capitalize ${types.includes("completed") ? "btn-active" : ""}`} onClick={() => toggleType("completed")}>completed</button>
+        </div>
+        <label className="input input-primary input-sm w-1/2">
+          <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
+          <input type="search" className="grow" onChange={(e) => setQuery(e.target.value)} placeholder="Search" />
+        </label>
+      </div>
         
       </div>
 
       <div className="bg-white rounded-lg shadow">
         <Table
           columns={columns}
-          dataSource={parkingT}
+          dataSource={items}
           rowKey="id"
           loading={{
             indicator: <Spin indicator={<Loader2 className="h-8 w-8 animate-spin text-primary" />} />,
